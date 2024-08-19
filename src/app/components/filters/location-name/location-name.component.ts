cde780dd482e5740ea98864c8a11372d0a894e15
Akhilesh Awachat <a.awachat@maxxton.onmicrosoft.com>
2024-08-20 00:53:02 +0530

    filters in place

--- src/app/components/filters/location-name/location-name.component.ts
+++ src/app/components/filters/location-name/location-name.component.ts
@@ -12,9 +12,6 @@ export class LocationNameComponent {
 
   constructor(private filterService: FilterService) {}
 
-  // @Output() locationChanged: EventEmitter<number> = new EventEmitter<number>();
-  // selectedLocation?: number;
-
   onLocationChange(value: string): void {
     const selectedLocation = parseInt(value);
     this.filterService.setLocationFilter(isNaN(selectedLocation) ? null : selectedLocation);
--- src/app/pages/customer-portal-home/customer-portal-home.component.ts
+++ src/app/pages/customer-portal-home/customer-portal-home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, forkJoin } from 'rxjs';
-import { RoomStayDetails } from '../../interface/room-stay-details';
-import { RoomDetails } from '../../interface/room-details';
 import { RoomAndRoomStayDetails } from '../../interface/room-and-room-stay-details';
 import { RoomDetailsApiService } from '../../service/apiService/room-details-api.service';
 import { MergeRoomAndRoomDetails } from '../../utils/merge-room-and-room-details.pipe';
@@ -13,22 +11,21 @@ import { FilterService } from '../../service/filterService/filter.service';
   styleUrl: './customer-portal-home.component.scss',
 })
 export class CustomerPortalHomeComponent implements OnInit {
-  public roomStayDetails$!: Observable<RoomStayDetails[]>;
-  public roomDetails$!: Observable<RoomDetails[]>;
   public roomAndRoomStayDetails : RoomAndRoomStayDetails[] = [];
   public roomDetailsForFilter : RoomAndRoomStayDetails[] = [];
   public viewRoomData : RoomAndRoomStayDetails = {} as RoomAndRoomStayDetails;
-  public roomAndRoomStayDetails$!: Observable<RoomAndRoomStayDetails[]>;
 
   constructor(
     private roomDetailsApiService: RoomDetailsApiService,
     private mergePipe: MergeRoomAndRoomDetails,
     private filterService: FilterService
-  ) {
-     this.roomAndRoomStayDetails$ = this.filterService.getFilteredRoomStayDetails();
-  }
+  ) {  }
 
   ngOnInit() {
+    this.fetchDataAndApplyFilters();
+  }
+  
+  private fetchDataAndApplyFilters() {
     forkJoin({
       roomStayDetails: this.roomDetailsApiService.getRoomStayDetails(),
       roomDetails: this.roomDetailsApiService.getRoomDetails(),
@@ -36,43 +33,21 @@ export class CustomerPortalHomeComponent implements OnInit {
       this.roomAndRoomStayDetails = this.mergePipe.transform(
         roomStayDetails,
         roomDetails
-      ),
-      this.roomDetailsForFilter = this.roomAndRoomStayDetails
-      console.log(this.roomAndRoomStayDetails);
-    });
-  }
-
-  onFilterChange(selectedLocation: number) {
-    try{
-      forkJoin({
-        roomStayDetails: this.roomDetailsApiService.getRoomStayDetails(),
-        roomDetails: this.roomDetailsApiService.getRoomDetails(),
-      }).subscribe(({ roomStayDetails, roomDetails }) => {
-        this.roomAndRoomStayDetails = this.mergePipe.transform(
-          roomStayDetails,
-          roomDetails
-        );
-        if(selectedLocation){
-          this.roomAndRoomStayDetails = this.roomAndRoomStayDetails?.filter(room => room.locationId == selectedLocation);
-        }
+      );
+      this.roomDetailsForFilter = this.roomAndRoomStayDetails;
+      this.filterService.setRoomStayDetails(this.roomAndRoomStayDetails);
+      this.filterService.getFilteredRoomStayDetails().subscribe((data) => {
+        this.roomAndRoomStayDetails = data;
+        console.log("roomDetailsForFilter",this.roomAndRoomStayDetails);
       });
-      
-    }
-    catch(error){
-      console.log(error);
-    }
-  }
-
-  onPriceChanged(newPrice: number) {
-    console.log('Price changed:', newPrice);
-    // Handle the price value here
-  }
-
-  onGuestChange(guestNo: number) {
-    console.log('Guest changed:', guestNo);
+    });
   }
 
   onViewRoom(viewRoomData: RoomAndRoomStayDetails) {
     this.viewRoomData = viewRoomData;
+  }
+
+  clearFilter(){
+    this.filterService.resetFilters();
   }
 }
